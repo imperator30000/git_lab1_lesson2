f48fc928fd5cd48cd3ad5ee586bf77cd624ca898
dangen-master <95441430+dangen-master@users.noreply.github.com>
2022-01-02 13:24:10 +0300

    Add files via upload

---
+++ drawing.py
@@ -0,0 +1,25 @@
+import pygame
+from settings import *
+from ray_castting import ray_casting
+
+
+class Drawing:
+    def __init__(self, sc):
+        self.sc = sc
+        self.font = pygame.font.SysFont("Arial", 36, bold=True)
+        self.texture = pygame.image.load("images/1.png").convert()
+
+    def background(self):
+        # Рисуем землю и небо
+        pygame.draw.rect(self.sc, SKYBLUE, (0, 0, WIDTH, HALF_HEIGHT))
+        pygame.draw.rect(self.sc, DARKGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
+
+    def world(self, player_pos, player_angle):
+        # Отрисовка стен
+        ray_casting(self.sc, player_pos, player_angle, self.texture)
+
+    def fps(self, clock):
+        # Счётчик кадров
+        display_fps = str(int(clock.get_fps()))
+        render = self.font.render(display_fps, 0, RED)
+        self.sc.blit(render, FPS_POS)
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import pygame
+from settings import *
+from player import Player
+from drawing import Drawing
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Основное рабочее окно
+pygame.init()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
 
+clock = pygame.time.Clock()  # Клас для определениея количества кадров в секунду
+player = Player()  # Игрок
+drawing = Drawing(sc)
+# Основной цикл программы
+while True:
+    # Проверка на закрытие программы
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    player.movement()  # Ходьба
+    # Черный фон
+    sc.fill(BLACK)
+    # Рисуем землю и небо
+    drawing.background()
+    # Рисуем стены
+    drawing.world(player.pos, player.angle)
+    # счётчик фпс
+    drawing.fps(clock)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Обновляем экран на каждой итэрации
+    pygame.display.flip()
+    clock.tick()
