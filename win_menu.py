92c01eac5c795bc6fe89dd5f812312790c82e6f2
vadim <Vadim09531@yandex.ru>
2022-01-07 23:10:32 +0300

    класс окон меню

--- win_menu.py
+++ win_menu.py
@@ -2,27 +2,169 @@ import pygame as pg
 
 
 class Obj:
-    def __init__(self, img, x_y,size, name):
-        self.img = img
+    def __init__(self, img, x_y, size, name, type_obj='title', text='', font_size=70, c=0, e=255, ch=True, ok=255,
+                 min_max=(11, 99), fun=lambda: ''):
+        self.img = pg.transform.scale(img, size).convert_alpha()
         self.x_y = x_y
         self.size = size
         self.name = name
-        self.c = 0
+        self.changing = ch
+        self.step = 5
+        self.end_counter = e
+        self.counter = c
+        self.koef = 1
+        self.type_obj = type_obj
+        self.ok_end = ok
+        self.dependent_objects = []
+        self.fun = fun
+        self.enabled = False
+        self.font = pg.font.SysFont('inkfree', font_size)
+        self.text = self.font.render(text, True, (0, 0, 0)).convert_alpha()
+        text_size = self.text.get_size()
+        self.text_x_y = [self.x_y[i] + (self.size[i] - text_size[i]) // 2 for i in range(2)]
+        self.text_ = text
+        self.min_max = min_max
+        self.recover()
+
+    def spin(self, x_y, step, min_, max_):
+        if self.type_obj == 'spin':
+            text_ = int(self.text_) + step
+            a = self.in_obj(x_y)
+            if min_ <= text_ <= max_ and a:
+                self.text_ = str(text_)
+                self.text = self.font.render(self.text_, True, (0, 0, 0)).convert_alpha()
+                self.text_x_y = [self.x_y[i] + (self.size[i] - self.text.get_size()[i]) // 2 for i in range(2)]
+                self.text.set_alpha(self.counter)
+            print(text_)
+
+    def in_obj(self, x_y):
+        a = [self.x_y[g] <= x_y[g] <= self.x_y[g] + self.size[g] for g in range(2)]
+        return all(a)
+
+    def recover(self, pressed=False):
+        self.go_animation(self.ok_end, 5)
+
+        for i in self.dependent_objects:
+            if not i.changing and (not i.enabled or pressed):
+                i.go_animation(self.ok_end, 5)
+            if pressed:
+                i.enabled = False
+
+        self.enabled = False
+
+    def choice(self, x_y):
+
+        if not self.enabled:
+            a = self.in_obj(x_y)
+            if a and self.type_obj in 'btn spin':
+                self.go_animation(150, 5)
+                for i in self.dependent_objects:
+                    if not i.enabled:
+                        i.go_animation(150, 5)
+            else:
+                self.recover()
+
+    def pressed(self, x_y, pressed):
+        a = self.in_obj(x_y)
+        if pressed and self.type_obj == 'btn' and a:
+            if not self.enabled:
+                self.enabled = True
+                self.go_animation(0, 5)
+                for i in self.dependent_objects:
+                    i.enabled = True
+                    i.go_animation(0, 5)
+
+                self.fun()
+
+            else:
+
+                self.recover(True)
+
+    def go_animation(self, end, step):
+        self.changing = True
+        self.step = step
+        self.end_counter = end
+        if self.counter < self.end_counter:
+            self.koef = 1
+        else:
+            self.koef = -1
+
+    def animation(self):
+        if self.changing:
+            self.counter += self.koef * self.step
+            if self.counter * self.koef >= self.end_counter * self.koef:
+                self.counter = self.end_counter
+                self.changing = False
+            self.text.set_alpha(self.counter)
+            self.img.set_alpha(self.counter)
+
 
 class Window:
-    def __init__(self, W_H):
+    def __init__(self, W_H, back):
         pg.init()
+        pg.font.init()
         self.sc = pg.display.set_mode(W_H)
-        self.objs = []
+        self.objs = {'back': Obj(back, (0, 0), W_H, 'back')}
+        self.black = Obj(pg.image.load('img/black.png'), (0, 0), W_H, 'black', ch=False, ok=0, e=0)
+        self.clock = pg.time.Clock()
+        self.m_action = [False, False, False, False]  # движение нажатие
+        self.m_pos = (-1, -1)
 
     def render(self):
-        [self.sc.blit(i.img, i.x_y) for i in self.objs]
+        a = [[self.sc.blit(self.objs[i].img, self.objs[i].x_y),
+              self.sc.blit(self.objs[i].text, self.objs[i].text_x_y)] for i in self.objs]
 
-    def check_obj(self, x, y):
-        arr = []
-        for i in self.objs:
-            if i.x_y[0] <= x <= i.x_y[0] + i.size[0]
+    def add_obj(self, obj, dependent_objects_name=()):
+        if obj.name in self.objs:
+            return False
+        self.objs[obj.name] = obj
+        dependent_objects = []
+        for i in dependent_objects_name:
+            dependent_objects.append(self.objs[i])
+        obj.dependent_objects = dependent_objects
+        self.objs['back'].dependent_objects.append(obj)
 
+    def run(self):
+        while True:
 
+            self.m_action = [False, False, False, False]
+            for i in pg.event.get():
+                if i.type == pg.QUIT:
+                    exit()
 
+                if i.type == pg.MOUSEBUTTONDOWN:
+                    if i.button == 1:
+                        self.m_action[1] = True
 
+                    if i.button == 4:
+                        self.m_action[2] = True
+
+                    if i.button == 5:
+                        self.m_action[3] = True
+
+                    print(i.button)
+                    self.m_pos = i.pos
+
+                if i.type == pg.MOUSEMOTION:
+                    self.m_action[0] = True
+                    self.m_pos = i.pos
+
+            for i in self.objs:
+                self.objs[i].animation()
+                if any(self.m_action[:2]):
+                    self.objs[i].choice(self.m_pos)
+                if self.m_action[1]:
+                    self.objs[i].pressed(self.m_pos, self.m_action[1])
+                if self.m_action[2]:
+                    self.objs[i].spin(self.m_pos, 1, 11, 99)
+                if self.m_action[3]:
+                    self.objs[i].spin(self.m_pos, -1, 11, 99)
+
+                # print(self.m_action)
+            self.render()
+
+            pg.display.flip()
+            self.clock.tick(60)
+
+# app = Window((1200, 800), pg.image.load('img/back.png'))
+# app.run()
