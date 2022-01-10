0baffe4b55f53196ffce26df42ed21c27ea754a3
vadim <Vadim09531@yandex.ru>
2022-01-10 16:59:47 +0300

    засунул игру в меню

--- main.py
+++ main.py
@@ -1,39 +1,79 @@
-import pygame
+
 from player import Player
 from drawing import *
 from ray_castting import ray_casting_walls
 
-# Основное рабочее окно
-pygame.init()
-sc = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.mouse.set_visible(False)
-clock = pygame.time.Clock()  # Клас для определениея количества кадров в секунду
-player = Player()  # Игрок
-drawing = Drawing(sc, clock)
-# Основной цикл программы
-# MAZE.update_sek(1)
 
-while True:
-    # Проверка на закрытие программы
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
 
-    player.movement(drawing)  # Ходьба
-    # Обновляем экран на каждой итэрации
-    pygame.display.flip()
-    # Черный фон
-    sc.fill(BLACK)
-    # Рисуем землю и небо
-    drawing.background()
-    walls = ray_casting_walls(player, drawing.textures)
-    # Рисуем стены
-    drawing.world(walls)
-    # minimap
-    drawing.minimap(player.pos, player.angle)
-    print(drawing.minimap_fill_quat())
-    # счётчик фпсD
-    drawing.fps(clock)
-    drawing.chek_win(player.pos)
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    clock.tick()
+        self.clock = pygame.time.Clock()  # Клас для определениея количества кадров в секунду
+        self.player = Player()  # Игрок
+        self.drawing = Drawing(self.screen, self.clock)
+
+
+    def run(self):
+        pygame.mouse.set_visible(False)
+        while True:
+            # Проверка на закрытие программы
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+
+            self.player.movement(self.drawing)  # Ходьба
+            # Обновляем экран на каждой итэрации
+            pygame.display.flip()
+            # Черный фон
+            self.screen.fill(BLACK)
+            # Рисуем землю и небо
+            self.drawing.background()
+            walls = ray_casting_walls(self.player, self.drawing.textures)
+            # Рисуем стены
+            self.drawing.world(walls)
+            # minimap
+            self.drawing.minimap(self.player.pos, self.player.angle)
+            print(self.drawing.minimap_fill_quat())
+            # счётчик фпсD
+            self.drawing.fps(self.clock)
+            self.drawing.chek_win(self.player.pos)
+
+            self.clock.tick()
+
+# # Основное рабочее окно
+# pygame.init()
+# sc = pygame.display.set_mode((WIDTH, HEIGHT))
+# pygame.mouse.set_visible(False)
+# clock = pygame.time.Clock()  # Клас для определениея количества кадров в секунду
+# player = Player()  # Игрок
+# drawing = Drawing(sc, clock)
+# # Основной цикл программы
+# # MAZE.update_sek(1)
+#
+# while True:
+#     # Проверка на закрытие программы
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             exit()
+#
+#     player.movement(drawing)  # Ходьба
+#     # Обновляем экран на каждой итэрации
+#     pygame.display.flip()
+#     # Черный фон
+#     sc.fill(BLACK)
+#     # Рисуем землю и небо
+#     drawing.background()
+#     walls = ray_casting_walls(player, drawing.textures)
+#     # Рисуем стены
+#     drawing.world(walls)
+#     # minimap
+#     drawing.minimap(player.pos, player.angle)
+#     print(drawing.minimap_fill_quat())
+#     # счётчик фпсD
+#     drawing.fps(clock)
+#     drawing.chek_win(player.pos)
+#
+#     clock.tick()
+win_game = Game()
--- main_menu.py
+++ main_menu.py
@@ -1,13 +1,15 @@
 from win_menu import Obj, Window
 import pygame as pg
-from Shader import PyShader
+# from Shader import PyShader
 from maze_back import MazeBack
+from main import win_game
 
 # , pg.image.load('img/back_.png'), 'name1'
 
 
 back = MazeBack
-back = PyShader
+# back = PyShader
+# пауза
 win_pause = Window((1200, 800), back)
 btn_pause = Obj(pg.image.load('img/brick.png'), (310, 350), (250, 100), 'pause', 'btn', 'pause')
 btn_restart = Obj(pg.image.load('img/brick.png'), (650, 350), (250, 100), 'restart', 'btn', 'restart')
@@ -15,6 +17,7 @@ win_pause.add_obj(btn_pause)
 win_pause.add_obj(btn_restart)
 
 # back = PyShader
+# меню
 win_menu = Window((1200, 800), back)
 btn_play = Obj(pg.image.load('img/brick_.png'), (310, 450), (270, 100), 'play', 'btn', 'play', fun=win_pause.run)
 btn_settings = Obj(pg.image.load('img/brick_.png'), (630, 450), (270, 100), 'settings', 'btn', 'settings', 60)
@@ -25,8 +28,7 @@ win_menu.add_obj(btn_settings)
 win_menu.add_obj(btn_title)
 btn_restart.fun = win_menu.run
 
-
-
+# кастом режим
 win_castom_mode = Window((1200, 800), back)
 title_castom_mode = Obj(pg.image.load('img/brick_.png'), (50, 50), (300, 100), 'Castom mode', 'title', 'Castom mode',
                         50, ok=150)
@@ -41,30 +43,31 @@ title_radius = Obj(pg.image.load('img/brick_.png'), (850, 50), (300, 100), 'Radi
 win_castom_mode.add_obj(title_radius)
 title_spin_radius = Obj(pg.image.load('img/brick_.png'), (950, 200), (100, 100), 'Spin radius', 'spin', '11')
 win_castom_mode.add_obj(title_spin_radius, ['Radius'])
-title_time = Obj(pg.image.load('img/brick_.png'), (850, 350), (300, 100), 'Time update', 'title', 'Time update',  50)
+title_time = Obj(pg.image.load('img/brick_.png'), (850, 350), (300, 100), 'Time update', 'title', 'Time update', 50)
 win_castom_mode.add_obj(title_time)
-title_spin_time = Obj(pg.image.load('img/brick_.png'), (950, 500), (100, 100), 'Spin time', 'spin', '900',40,step_spin=10,
+title_spin_time = Obj(pg.image.load('img/brick_.png'), (950, 500), (100, 100), 'Spin time', 'spin', '900', 40,
+                      step_spin=10,
                       min_max=(100, 1000))
 win_castom_mode.add_obj(title_spin_time, ['Time update'])
 
-
-
-
+# хард режим
 win_hard_mode = Window((1200, 800), back)
 title_hard_mode.fun = win_hard_mode.run
 title_castom_mode = Obj(pg.image.load('img/brick_.png'), (50, 50), (300, 100), 'Castom mode', 'btn', 'Castom mode',
                         50, fun=win_castom_mode.run)
 win_hard_mode.add_obj(title_castom_mode)
-title_hard_mode = Obj(pg.image.load('img/brick_.png'), (50, 200), (300, 100), 'Hard mode', 'title', 'Hard mode', 50, ok=150)
+title_hard_mode = Obj(pg.image.load('img/brick_.png'), (50, 200), (300, 100), 'Hard mode', 'title', 'Hard mode', 50,
+                      ok=150)
 win_hard_mode.add_obj(title_hard_mode)
-title_play_ = Obj(pg.image.load('img/brick_.png'), (50, 350), (300, 100), 'Play', 'btn', 'Play', fun=win_pause.run)
+title_play_ = Obj(pg.image.load('img/brick_.png'), (50, 350), (300, 100), 'Play', 'btn', 'Play', fun=win_game.run)
 win_hard_mode.add_obj(title_play_)
 title_menu = Obj(pg.image.load('img/brick_.png'), (50, 650), (300, 100), 'Menu', 'btn', 'Menu')
 win_hard_mode.add_obj(title_menu)
 title_radius = Obj(pg.image.load('img/brick_.png'), (850, 50), (300, 100), 'Hard', 'title', 'Hard')
 win_hard_mode.add_obj(title_radius)
-title_spin_radius = Obj(pg.image.load('img/brick_.png'), (950, 200), (100, 100), 'Spin hard', 'spin', '1', min_max=(1, 20))
+title_spin_radius = Obj(pg.image.load('img/brick_.png'), (950, 200), (100, 100), 'Spin hard', 'spin', '1',
+                        min_max=(1, 20))
 win_hard_mode.add_obj(title_spin_radius, ['Hard'])
 btn_play.fun = win_hard_mode.run
-
+# запуск меню
 win_menu.run()
